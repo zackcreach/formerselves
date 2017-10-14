51c485e5f78aae38364225c3ca9579571577c65c
zackcreach <zackcreach@gmail.com>
2017-10-14 16:52:08 -0400

    Fixed animation.

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -12,37 +12,37 @@ export default class Main extends Component {
       videoURL: 'https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761',
       emailAddress: '',
       placeholder: 'Email Address',
-      buttonDisabled: true
+      buttonDisabled: true,
+      inputDisabled: false,
     }
   }
   _handleChange(e) {
-    if (/(^\w.*?@.*?\.\w)/g.test(e.target.value)) {
+    if (/(^\w.*@\w+\.\w)/g.test(e.target.value)) {
       console.log('%cValid email!', 'font-size: 16px; color: black; font-weight: bold')
-      this.setState({
-        buttonDisabled: false
-      })
+      this.setState({buttonDisabled: false});
     } else {
       console.log('%cKeep going...', 'font-size: 16px; color: red; font-weight: bold')
+      this.setState({buttonDisabled: true});
     }
   }
   _handleSubmit(e) {
     e.preventDefault();
     this.emailAddress.style.background = 'green';
-
     // capture email inputted in an object with a timestamp property, formatted using moment
-    const timestamp = moment().format('MMMM Do YYYY, h:mm:ss A');
+    // const timestamp = moment().format('MMMM Do YYYY, h:mm:ss A');
+    // Using just Date.now() as we want the raw timestamp for later
+    const timestamp = Date.now();
     const email = {
       emailAddress: this.emailAddress.value,
       timestamp
     };
-
     // Update the state for the individual address submitted
     this.setState({
       emailAddress: this.emailAddress.value,
-      placeholder: 'Submitted',
-      buttonDisabled: true
+      placeholder: 'Submitted! Stay tuned.',
+      buttonDisabled: true,
+      inputDisabled: true,
     });
-
     base.push('emails', {
       data: {email},
       then(err) {
@@ -54,13 +54,14 @@ export default class Main extends Component {
     this.emailForm.reset();
   }
   render() {
-    let emailToggle = this.state.buttonDisabled ? 'Main__email Main__email--full' : 'Main__email'
-    let buttonToggle = this.state.buttonDisabled ? 'Main__button Main__button--disabled' : 'Main__button'
+    const { videoURL, placeholder, buttonDisabled, inputDisabled } = this.state;
+    let emailToggle = buttonDisabled ? 'Main__email Main__email--full' : 'Main__email';
+    let buttonToggle = buttonDisabled ? 'Main__button Main__button--disabled' : 'Main__button';
     return(
       <div className='Main'>
         <div className='Main__video-container'>
           <video className='Main__video' muted autoPlay loop>
-            <source src={this.state.videoURL} type='video/mp4'></source>
+            <source src={videoURL} type='video/mp4'></source>
           </video>
         </div>
         <div className='Main__mask'></div>
@@ -74,15 +75,15 @@ export default class Main extends Component {
               onChange={(e) => this._handleChange(e)}
             >
               <input 
-                className={emailToggle}
                 ref={(input) => {this.emailAddress = input}} 
-                placeholder={this.state.placeholder}
-                /* disabled={this.state.buttonDisabled} */
+                placeholder={placeholder}
+                className={emailToggle}
+                disabled={inputDisabled}
               />
               <input 
                 type='submit'
                 className={buttonToggle}
-                disabled={this.state.buttonDisabled} 
+                disabled={buttonDisabled} 
               />
             </form>
           </div>

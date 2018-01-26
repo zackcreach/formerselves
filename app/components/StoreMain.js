6ffd79ae4c25f37d23c9ee1a5d4444900b4117f4
Zack Creach <zackcreach@gmail.com>
2018-01-26 14:19:24 -0500

    Formatting complete.

--- app/components/StoreMain.js
+++ app/components/StoreMain.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'styles/styles';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "styles/styles";
 
-import StoreNav from 'StoreNav';
-import StoreBrowse from 'StoreBrowse';
+import StoreNav from "StoreNav";
+import StoreBrowse from "StoreBrowse";
 
 export default class StoreMain extends Component {
   constructor() {
@@ -23,26 +23,10 @@ export default class StoreMain extends Component {
         <Route path="/store/faq" component={Faq} />
         <Route path="/store/contact" component={Contact} />
       </div>
-    )
+    );
   }
 }
-const Lookbooks = () => (
-  <div>
-    Lookbooks
-  </div>
-)
-const Press = () => (
-  <div>
-    Press
-  </div>
-)
-const Faq = () => (
-  <div>
-    Faq
-  </div>
-)
-const Contact = () => (
-  <div>
-    Contact
-  </div>
-)
+const Lookbooks = () => <div>Lookbooks</div>;
+const Press = () => <div>Press</div>;
+const Faq = () => <div>Faq</div>;
+const Contact = () => <div>Contact</div>;
--- app/components/StoreNav.js
+++ app/components/StoreNav.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link, NavLink } from "react-router-dom";
 
-import 'styles/styles';
-import fsLogo from 'images/fs-logo--black';
+import "styles/styles";
+import fsLogo from "images/fs-logo--black";
 
 export default class StoreNav extends Component {
   constructor() {
@@ -12,23 +12,53 @@ export default class StoreNav extends Component {
     return (
       <div className="StoreNav">
         <div className="StoreNav__image-container">
-          <img src={fsLogo} alt="Former Selves" className="StoreNav__image"/>
+          <img src={fsLogo} alt="Former Selves" className="StoreNav__image" />
         </div>
         <ul className="StoreNav__list">
           <li className="StoreNav__list-item">
-            <NavLink className="StoreNav__link" activeClassName="StoreNav__link--active" to="/store/shop">Shop</NavLink>
+            <NavLink
+              className="StoreNav__link"
+              activeClassName="StoreNav__link--active"
+              to="/store/shop"
+            >
+              Shop
+            </NavLink>
           </li>
           <li className="StoreNav__list-item">
-            <NavLink className="StoreNav__link" activeClassName="StoreNav__link--active" to="/store/lookbooks">Lookbooks</NavLink>
+            <NavLink
+              className="StoreNav__link"
+              activeClassName="StoreNav__link--active"
+              to="/store/lookbooks"
+            >
+              Lookbooks
+            </NavLink>
           </li>
           <li className="StoreNav__list-item">
-            <NavLink className="StoreNav__link" activeClassName="StoreNav__link--active" to="/store/press">Press</NavLink>
+            <NavLink
+              className="StoreNav__link"
+              activeClassName="StoreNav__link--active"
+              to="/store/press"
+            >
+              Press
+            </NavLink>
           </li>
           <li className="StoreNav__list-item">
-            <NavLink className="StoreNav__link" activeClassName="StoreNav__link--active" to="/store/faq">FAQ</NavLink>
+            <NavLink
+              className="StoreNav__link"
+              activeClassName="StoreNav__link--active"
+              to="/store/faq"
+            >
+              FAQ
+            </NavLink>
           </li>
           <li className="StoreNav__list-item">
-            <NavLink className="StoreNav__link" activeClassName="StoreNav__link--active" to="/store/contact">Contact</NavLink>
+            <NavLink
+              className="StoreNav__link"
+              activeClassName="StoreNav__link--active"
+              to="/store/contact"
+            >
+              Contact
+            </NavLink>
           </li>
         </ul>
       </div>
